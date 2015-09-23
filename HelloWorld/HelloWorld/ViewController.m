9532b11d1c9757c808344ce1ba8f03663c54b488
polly3d <polly3d@126.com>
2015-09-23 12:03:25 +0800

    hello world

--- HelloWorld/HelloWorld/ViewController.m
+++ HelloWorld/HelloWorld/ViewController.m
@@ -9,16 +9,46 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *label;
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
 @property (weak, nonatomic) IBOutlet UITextField *textfield;
+@property (nonatomic) NSString *string;
+
 @end
 
 @implementation ViewController
 
+- (void)updateString
+{
+	self.string = textfield.text;
+	label.text = self.string;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
+    // When the user presses return, take focus away from the text field so that the keyboard is dismissed.
+    if (theTextField == textField) {
+        [textField resignFirstResponder];
+        // Invoke the method that changes the greeting.
+        [self updateString];
+    }
+    return YES;
+}
+ 
+ 
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    // Dismiss the keyboard when the view outside the text field is touched.
+    [textField resignFirstResponder];
+    // Revert the text field to the previous value.
+    textField.text = self.string; 
+    [super touchesBegan:touches withEvent:event];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    textfield.clearButtonMode = UITextFieldViewModeWhileEditing;
+    label.text = textfield.placeholder;
 }
 
 - (void)didReceiveMemoryWarning {
